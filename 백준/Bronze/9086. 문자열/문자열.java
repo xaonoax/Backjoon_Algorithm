a1348564532c216f15e971671a00e78a67531188
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2023-09-07 16:56:23 +0900

    [Bronze V] Title: 문자열, Time: 112 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i=0; i<t; i++) {
+            String str = sc.next();
+            System.out.println(str.charAt(0) + "" + str.charAt(str.length() - 1));
+        }
+
+    }
+}
