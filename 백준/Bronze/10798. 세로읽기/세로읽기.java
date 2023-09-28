49f7b55f41bcb2a5695cdbd117c07fe524229e82
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2023-09-29 08:37:22 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char[][] arr = new char[5][15];
+
+        for(int i=0; i<5; i++) {
+            String s = sc.next();
+
+            for(int j=0; j<s.length(); j++) {
+                arr[i][j] = s.charAt(j);
+            }
+        }
+
+        for(int i=0; i<15; i++) {
+            for(int j=0; j<5; j++) {
+                if(arr[j][i] == ' ' || arr[j][i] == '\0') {
+                    continue;
+                }
+                System.out.print(arr[j][i]);
+            }
+        }
+
+    }
+}
