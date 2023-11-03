05ffe0b4de5c8d71f8bc0892a4f08ef8972b7521
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2023-11-03 21:07:33 +0900

    [level unrated] Title: 주사위의 개수, Time: 0.02 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.java
@@ -1,10 +1,7 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = 1;
-
-        for(int i=0; i<box.length; i++) {
-            answer *= box[i] / n;
-        }
+        int answer = (box[0] / n) * (box[1] / n) * (box[2] / n);
+        
         return answer;
     }
 }
