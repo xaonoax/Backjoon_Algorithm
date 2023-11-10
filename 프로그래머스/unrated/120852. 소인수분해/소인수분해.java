4a71a78589c2ab0e2e02ed10565115b98dd75f72
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2023-11-10 15:27:11 +0900

    [level unrated] Title: 소인수분해, Time: 0.05 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=2; i<=n; i++) {
+            while (n % i == 0) {
+                if(!list.contains(i)) {
+                    list.add(i);
+                }
+                n /= i;
+            }
+        }
+
+        int[] answer = new int[list.size()];
+
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+
+        }
+
+        return answer;
+    }
+}
