9dc5c7250926f8f5980baf2505523382cd766f04
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-01-05 18:22:23 +0900

    [level unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -3,13 +3,11 @@ class Solution {
         int answer = 0;
 
         for (int i = 0; i < order.length; i++) {
-            int price = 0;
             if (order[i].contains("americano") || order[i].contains("anything")) {
-                price += 4500;
+                answer += 4500;
             } else if (order[i].contains("cafelatte")) {
-                price += 5000;
+                answer += 5000;
             }
-            answer += price;
         }
 
         return answer;
