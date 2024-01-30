2b3f53ef4c285a10e8f3c2f0e337f03cabb9bbb0
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-01-30 13:52:14 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String num = String.valueOf(order);
+
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+
+            if (c == '3' || c == '6' || c == '9') {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
