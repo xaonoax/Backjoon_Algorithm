30ee83f8c6ad251a235ffe5b3bb8a27b194ce2ba
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-02-18 20:45:54 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+
+        for (char c : my_string.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                answer[c - 'A']++;
+            } else if (Character.isLowerCase(c)){
+                answer[c - 'a' + 26]++;
+            }
+        }
+
+        return answer;
+    }
+}
