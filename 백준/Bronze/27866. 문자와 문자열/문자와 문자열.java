3b530e41c87890ec890f69087e08cf3371c609fa
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-04-19 17:06:46 +0900

    [Bronze V] Title: 문자와 문자열, Time: 112 ms, Memory: 12852 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] s = sc.next().split("");
-        int i = sc.nextInt();
+        String s = sc.next();
+        int b = sc.nextInt();
 
-        System.out.println(s[i-1]);
+        System.out.println(s.charAt(b - 1));
     }
 }
