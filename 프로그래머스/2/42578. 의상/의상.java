5d67f3aa2cb21eb6ad3294ff13edaef7140f1e6b
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-08-10 00:42:46 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+
+        answer -= 1;
+
+        return answer;
+    }
+}
