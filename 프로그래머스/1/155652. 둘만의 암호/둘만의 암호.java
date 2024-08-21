3bfc29248d19196502374fab0022698611d7d0a6
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-08-21 21:06:46 +0900

    [level 1] Title: 둘만의 암호, Time: 13.92 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        char ch = 'a';
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            for (int j = 0; j < index; j++) {
+                c += 1;
+                if (c > 'z') {
+                    c -= 26;
+                }
+                if (skip.contains(String.valueOf(c))) {
+                    j--;
+                }
+            }
+
+            answer += c;
+        }
+
+        return answer;
+    }
+}
