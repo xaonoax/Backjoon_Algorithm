5ca704832551b64159988127e95f18e992f1a6f6
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-09-19 23:04:27 +0900

    [level 2] Title: 큰 수 만들기, Time: 8115.49 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        int idx = 0;
+
+        for (int i = 0; i < number.length() - k; i++) {
+            char max = 0;
+
+            for (int j = idx; j <= i + k; j++) {
+                if (max < number.charAt(j)) {
+                    max = number.charAt(j);
+                    idx = j + 1;
+                }
+            }
+            answer.append(max);
+        }
+
+        return answer.toString();
+    }
+}
