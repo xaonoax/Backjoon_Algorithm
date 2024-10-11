ae51cc3b41859354d0df2aef24549f264a261727
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-10-11 22:55:47 +0900

    [Bronze II] Title: 벌집, Time: 92 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+        int range = 2;
+
+        if (n == 1) {
+            System.out.println(1);
+        } else {
+            while (range <= n) {
+                range = range + (6 * count);
+                count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
