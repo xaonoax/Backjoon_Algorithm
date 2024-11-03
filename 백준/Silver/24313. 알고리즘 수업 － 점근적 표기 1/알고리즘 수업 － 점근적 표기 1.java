646839474f94bdf526c75103fa602fc06f47f851
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-11-03 23:37:48 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 96 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a0 = sc.nextInt();
+        int c = sc.nextInt();
+        int n0 = sc.nextInt();
+
+        if ((a1 * n0 + a0) <= c * n0 && a1 <= c) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
