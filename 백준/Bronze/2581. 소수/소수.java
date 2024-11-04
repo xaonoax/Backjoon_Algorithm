98797ccd7aabe331b9eba8d6d1841e7a71ac38c1
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-11-04 23:58:59 +0900

    [Bronze II] Title: 소수, Time: 264 ms, Memory: 12932 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
+        int min = n + 1;
+
+        for (int i = m; i <= n; i++) {
+            int count = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                sum += i;
+                if (min > i) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
