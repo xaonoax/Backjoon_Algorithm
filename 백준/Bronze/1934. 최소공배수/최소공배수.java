6f31f7d320aef3bacd905b03f9c9435dc00433a8
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-11-19 23:17:59 +0900

    [Bronze I] Title: 최소공배수, Time: 204 ms, Memory: 19392 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a * b / gcd(a, b));
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
