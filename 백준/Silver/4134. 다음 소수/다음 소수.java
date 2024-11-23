c21c7d01554f6fce09c5c10ab7b99398b8d96d82
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2024-11-23 22:10:50 +0900

    [Silver IV] Title: 다음 소수, Time: 488 ms, Memory: 17048 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (long i = 0; i < n; i++) {
+            long a = sc.nextLong();
+
+            while (true) {
+                long count = 0;
+
+                for (long j = 2; j <= Math.sqrt(a); j++) {
+                    if (a % j == 0) {
+                        count++;
+                        break;
+                    }
+                }
+
+                if (count == 0) {
+                    if (a == 0 || a == 1) {
+                        a = 2;
+                    }
+                    System.out.println(a);
+                    break;
+                }
+                a++;
+            }
+        }
+    }
+}
