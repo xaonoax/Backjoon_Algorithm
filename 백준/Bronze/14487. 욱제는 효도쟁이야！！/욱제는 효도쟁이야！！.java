0f6697e44095ccaa52945d03a665f02443201c16
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2025-03-31 23:41:03 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 468 ms, Memory: 57412 KB -BaekjoonHub

---
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 1; i++) {
+            count += arr[i];
+        }
+
+        System.out.println(count);
+    }
+}
