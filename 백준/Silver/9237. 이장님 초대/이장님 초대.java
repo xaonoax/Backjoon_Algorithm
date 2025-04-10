a40f556ce5c395f398ccbb7d23e9c4f52cee6496
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2025-04-10 23:51:46 +0900

    [Silver V] Title: 이장님 초대, Time: 920 ms, Memory: 95368 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, Comparator.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (i + 1) + (arr[i]);
+        }
+
+        Arrays.sort(arr, Comparator.reverseOrder());
+
+        System.out.println(arr[0] + 1);
+    }
+}
