0f424e837a67ee2298866fb0fa718e2d9f04fb3f
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2025-04-29 23:43:16 +0900

    [Bronze I] Title: 약수, Time: 92 ms, Memory: 12928 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        Arrays.sort(num);
+
+        System.out.println(num[0] * num[n - 1]);
+    }
+}
