b12fb600e7847a8e06f2a096acd360d288689236
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2025-05-10 23:45:31 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static boolean[][] arr;
+    public static int min = 64;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+
+            for (int j = 0; j < m; j++) {
+                if (str.charAt(j) == 'W') {
+                    arr[i][j] = true;
+                } else {
+                    arr[i][j] = false;
+                }
+            }
+        }
+
+        int n_row = n - 7;
+        int m_col = m - 7;
+
+        for (int i = 0; i < n_row; i++) {
+            for (int j = 0; j < m_col; j++) {
+                find(i, j);
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    public static void find(int x, int y) {
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int count = 0;
+
+        boolean flag = arr[x][y];
+
+        for (int i = x; i < end_x; i++) {
+            for (int j = y; j < end_y; j++) {
+                if (arr[i][j] != flag) {
+                    count++;
+                }
+                flag = (!flag);
+            }
+
+            flag = !flag;
+        }
+        count = Math.min(count, 64 - count);
+
+        min = Math.min(min, count);
+    }
+}
