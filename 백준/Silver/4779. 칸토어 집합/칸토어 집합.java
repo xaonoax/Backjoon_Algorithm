73f195651dc67a20d6fb5ea78264521e4e9acc5a
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2025-05-28 23:56:47 +0900

    [Silver III] Title: 칸토어 집합, Time: 176 ms, Memory: 21576 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static StringBuilder answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        
+        while ((str = br.readLine()) != null) {
+            n = Integer.parseInt(str);
+            answer =new StringBuilder();
+            int len = (int) Math.pow(3, n);
+            
+            for (int i = 0; i < len; i++) {
+                answer.append("-");
+            }
+
+            func(0, len);
+            
+            System.out.println(answer);
+
+        }
+    }
+
+    public static void func(int start,int size) {
+        if (size == 1) {
+            return;
+        }
+
+        int newSize = size / 3;
+        
+        for (int i = start + newSize; i < start + 2 * newSize; i++) {
+            answer.setCharAt(i, ' ');
+        }
+
+        func(start, newSize);
+        func(start + 2 * newSize, newSize);
+    }
+}
