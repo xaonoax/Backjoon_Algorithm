02179edca9bee9a00557816069a6a483aaad75b7
Ji-0 <130297565+xaonoax@users.noreply.github.com>
2025-09-09 23:30:36 +0900

    [Gold II] Title: 미확인 도착지, Time: 404 ms, Memory: 60216 KB -BaekjoonHub

---
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
@@ -0,0 +1,118 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Node implements Comparable<Node> {
+
+        int v, weight;
+
+        public Node(int v, int weight) {
+            this.v = v;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return weight - o.weight;
+        }
+    }
+
+    static final int INF = 100_000_000;
+    static int T;
+    static int V, E, t;
+    static List<List<Node>> list;
+    static int[] dist;
+    static List<Integer> candidate;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            t = Integer.parseInt(st.nextToken());
+            dist = new int[V + 1];
+
+            Arrays.fill(dist, INF);
+
+            list = new ArrayList<>();
+
+            for (int j = 0; j < V + 1; j++) {
+                list.add(new ArrayList<>());
+            }
+
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < E; j++) {
+                st = new StringTokenizer(br.readLine());
+
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                int weight = Integer.parseInt(st.nextToken());
+
+                if ((u == g && v == h) || (u == h && v == g)) {
+                    list.get(u).add(new Node(v, weight * 2 - 1));
+                    list.get(v).add(new Node(u, weight * 2 - 1));
+                } else {
+                    list.get(u).add(new Node(v, weight * 2));
+                    list.get(v).add(new Node(u, weight * 2));
+                }
+            }
+
+            candidate = new ArrayList<>();
+
+            for (int j = 0; j < t; j++) {
+                candidate.add(Integer.parseInt(br.readLine()));
+            }
+
+            dijkstra(start);
+
+            Collections.sort(candidate);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int num : candidate) {
+                if (dist[num] % 2 == 1) {
+                    sb.append(num).append(" ");
+                }
+            }
+
+            System.out.println(sb.toString().trim());
+        }
+    }
+
+    private static void dijkstra(int u) {
+        boolean[] visited = new boolean[V + 1];
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        dist[u] = 0;
+
+        pq.add(new Node(u, 0));
+
+        while (!pq.isEmpty()) {
+            Node currentNode = pq.poll();
+
+            if (visited[currentNode.v]) {
+                continue;
+            }
+
+            visited[currentNode.v] = true;
+
+            for (Node node : list.get(currentNode.v)) {
+                if (!visited[node.v] && dist[node.v] > dist[currentNode.v] + node.weight) {
+                    dist[node.v] = dist[currentNode.v] + node.weight;
+
+                    pq.add(new Node(node.v, dist[node.v]));
+                }
+            }
+        }
+    }
+}
